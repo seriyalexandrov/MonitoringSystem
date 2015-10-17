815036549a85dc3a8e94c5b33fbf8f0ba036a3d5
RiderOnTheStorm <vela.mipt@gmail.com>
2015-10-17 20:36:10 +0300

    Revert "New Function Added"
    This reverts commit 7fdfbe8eb93520d5a3af8ce09c23b6ed48aeeae8.

--- src/main/java/com/kalashnikov/monitoring/SystemExecutor.java
+++ src/main/java/com/kalashnikov/monitoring/SystemExecutor.java
@@ -4,14 +4,5 @@ public class SystemExecutor {
 
     public static void main(String[] args) {
         System.out.println("Executed!");
-        System.out.println("I love java");
-        System.out.println("abc");
-        PrintSum();
-    }
-
-    private static void PrintSum() {
-        int a = 5;
-        int b = 4;
-        System.out.println(a + b);
     }
 }
