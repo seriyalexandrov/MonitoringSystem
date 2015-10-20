2154cad3de19caf89d56a188e196afa0175eb0d4
OrEvg <evgenii.orekhov@phystech.edu>
2015-10-20 21:27:11 +0400

    commit test

--- src/main/java/com/kalashnikov/monitoring/SystemExecutor.java
+++ src/main/java/com/kalashnikov/monitoring/SystemExecutor.java
@@ -5,5 +5,6 @@ public class SystemExecutor {
     public static void main(String[] args) {
         System.out.println("Executed!");
         System.out.println("Hello ");
+        System.out.println("Goodbye");
     }
 }
