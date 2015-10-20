33c3193bc68a974a8454d1f55da96f8f4719ab2b
OrEvg <evgenii.orekhov@phystech.edu>
2015-10-20 21:28:11 +0400

    Revert "commit test"
    This reverts commit 2154cad3de19caf89d56a188e196afa0175eb0d4.

--- src/main/java/com/kalashnikov/monitoring/SystemExecutor.java
+++ src/main/java/com/kalashnikov/monitoring/SystemExecutor.java
@@ -5,6 +5,5 @@ public class SystemExecutor {
     public static void main(String[] args) {
         System.out.println("Executed!");
         System.out.println("Hello ");
-        System.out.println("Goodbye");
     }
 }
