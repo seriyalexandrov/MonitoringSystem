bfb2c08e0dc5dc6882aa97178542ca067f327be5
Sergei <seriyalexandrov@mail.ru>
2015-10-23 23:30:05 +0300

    Unit Test example.

---
+++ src/tests/source/JustATestExample.java
@@ -0,0 +1,30 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JustATestExample {
+
+    public static final int OPERATION_VALUE = 2;
+    private int a;
+    private int b;
+
+    @Before
+    public void initTest() {
+        a = 1;
+        b = 1;
+    }
+
+    @Test
+    public void performTest() {
+        int c = a + b;
+        assertEquals(c, OPERATION_VALUE);
+    }
+
+    @After
+    public void finishTest() {
+        a = 0;
+        b = 0;
+    }
+}
