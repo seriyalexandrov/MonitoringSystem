4d4811ca9622ddf3dae83950b33441f67036fbe6
RiderOnTheStorm <vela.mipt@gmail.com>
2015-10-26 02:14:18 +0300

    Replaced properties, deleted test-class OrderLogic

--- src/main/java/com/kalashnikov/monitoring/SystemExecutor.java
+++ src/main/java/com/kalashnikov/monitoring/SystemExecutor.java
@@ -3,12 +3,30 @@ package com.kalashnikov.monitoring;
 import com.kalashnikov.monitoring.config.jaxb.Option;
 import com.kalashnikov.monitoring.config.jaxb.Options;
 import com.kalashnikov.monitoring.config.jaxb.configurator.Configurator;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 public class SystemExecutor {
 
-    private static OrderLogic logic;
+    // »нициализаци€ логера : аргумент может быть любым названием которое
+    //ћы захотим выбрать. ¬ данном случае передаетс€ название класса
+    //Ёто удобно : так мы легко можем пон€ть где произошла ошибка
+    private static final Logger log = Logger.getLogger(SystemExecutor.class);
+    //private static final Logger log = Logger.getLogger("bacon"); --возможный вариант
+
 
     public static void main(String[] args) {
+
+        //Here is how we do logging :
+        //BasicConfigurator.configure();
+        //debug message
+        //log.debug("This is a debug message");
+        log.info("This is informative message!");
+        try {
+            throw new Exception("My Exception");
+        } catch (Exception e) {
+            log.error("This is an exception", e);
+        }
 
         System.out.println("Executed!");
         System.out.println("Hello ");
@@ -37,11 +55,6 @@ public class SystemExecutor {
             System.out.println(configurator.unMarshaller().getOptions().get(i).getName()
                     + ": " + configurator.unMarshaller().getOptions().get(i).getValue());
         }
-
-        //Here is how we do logging :
-        System.out.println("\nLet's try to check some logging :\n");
-        logic = new OrderLogic();
-        logic.doOrder();
 
     }
 
