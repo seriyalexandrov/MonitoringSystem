0d8ddf6b85fabc7788f46b1f11d6d83a43d21f96
Sergei <seriyalexandrov@mail.ru>
2015-10-29 00:25:51 +0300

    -log added to gitignore -Unit test fixed

--- src/main/tests/source/ConfiguratorTest.java
+++ src/main/tests/source/ConfiguratorTest.java
@@ -1,3 +1,6 @@
+import com.kalashnikov.monitoring.config.jaxb.Option;
+import com.kalashnikov.monitoring.config.jaxb.Options;
+import com.kalashnikov.monitoring.config.jaxb.configurator.Configurator;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -5,34 +8,38 @@ import java.lang.String;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class ConfiguratorTest {
+
+    Options listOfOptions = new Options();
+    private List<String> namesOfOptions = new ArrayList<String>();
+    private List<String> valuesOfOptions = new ArrayList<String>();
 
     @Before
     public void initTest() {
 
-        List<String> namesOfOptions = new ArrayList<String>();
-        nameOfOptions.add("Option1");
-        nameOfOptions.add("Option2");
-        nameOfOptions.add("Option3");
 
-        List<String> valuesOfOptions = new ArrayList<String>();
-        nameOfOptions.add("true");
-        nameOfOptions.add("read only");
-        nameOfOptions.add("1");
+        namesOfOptions.add("Option1");
+        namesOfOptions.add("Option2");
+        namesOfOptions.add("Option3");
+
+        valuesOfOptions.add("true");
+        valuesOfOptions.add("read only");
+        valuesOfOptions.add("1");
 
         Option option1 = new Option();
-        option1.setName(nameOfOptions.get(0));
-        option1.setValue(valueOfOptions.get(0));
+        option1.setName(namesOfOptions.get(0));
+        option1.setValue(valuesOfOptions.get(0));
 
         Option option2 = new Option();
-        option2.setName(nameOfOptions.get(1));
-        option2.setValue(valueOfOptions.get(1));
+        option2.setName(namesOfOptions.get(1));
+        option2.setValue(valuesOfOptions.get(1));
 
         Option option3 = new Option();
-        option3.setName(nameOfOptions.get(2));
-        option3.setValue(valueOfOptions.get(2));
+        option3.setName(namesOfOptions.get(2));
+        option3.setValue(valuesOfOptions.get(2));
 
-        Options listOfOptions = new Options();
         listOfOptions.addOption(option1);
         listOfOptions.addOption(option2);
         listOfOptions.addOption(option3);
