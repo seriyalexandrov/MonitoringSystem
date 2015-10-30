c83df3e89e70424eeef28bb4709766aaeaece2fe
RiderOnTheStorm <vela.mipt@gmail.com>
2015-10-30 21:59:34 +0300

    Apache Xml v1.2

--- src/main/java/com/kalashnikov/monitoring/SystemExecutor.java
+++ src/main/java/com/kalashnikov/monitoring/SystemExecutor.java
@@ -4,19 +4,10 @@ import org.apache.log4j.Logger;
 
 public class SystemExecutor {
 
-    // ������������� ������ : �������� ����� ���� ����� ��������� �������
-    //�� ������� �������. � ������ ������ ���������� �������� ������
-    //��� ������ : ��� �� ����� ����� ������ ��� ��������� ������
     private static final Logger log = Logger.getLogger(SystemExecutor.class);
-    //private static final Logger log = Logger.getLogger("bacon"); --��������� �������
-
 
     public static void main(String[] args) {
 
-        //Here is how we do logging :
-        //BasicConfigurator.configure();
-        //debug message
-        //log.debug("This is a debug message");
         log.info("This is informative message!");
         try {
             throw new Exception("My Exception");
