75a9cbdc3aada18c77c66836f32f21014e9696a5
RiderOnTheStorm <vela.mipt@gmail.com>
2015-10-31 15:18:26 +0300

    Upadated Apache XML Configurator v1.3

--- src/main/java/com/kalashnikov/monitoring/config/apache/xml/Configurator.java
+++ src/main/java/com/kalashnikov/monitoring/config/apache/xml/Configurator.java
@@ -3,26 +3,24 @@ package com.kalashnikov.monitoring.config.apache.xml;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.log4j.Logger;
-import org.w3c.dom.Document;
 
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.StringWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Configurator {
 
     private static final Logger log = Logger.getLogger(Configurator.class);
 
 
-    public String xmlSimpleReader() {
+    public Map<String, String> xmlConfiguratorReader() {
+
+        //creating an object to contain content of XML-file
+        Map<String, String> options = new LinkedHashMap<String, String>();
 
         //creating base for our XML file
         XMLConfiguration config = null;
         try {
-            config = new XMLConfiguration("const.xml");
+            config = new XMLConfiguration("config.xml");
         } catch (ConfigurationException e) {
             log.error("Error ocurred while config was created");
         }
@@ -34,32 +32,15 @@ public class Configurator {
          *  int i  - counter
          */
         int i = 0;
-        while(config.containsKey("databases.database(" + i + ").url")) {
-            config.getString("databases.database(" + i + ").url");
+
+        while(config.containsKey("option(" + i + ").option_name")) {
+            String option_name = config.getString("option(" + i + ").option_name");
+            String option_value = config.getString("option(" + i + ").option_value");
+            options.put(option_name, option_value);
             i++;
         }
 
-        //config lets us to get Document by calling its function getDocument
-        Document document = config.getDocument();
-        String xmlDocument = docToString(document);
-        return xmlDocument;
-    }
-
-    public String docToString(Document doc) {
-        try {
-            StringWriter sw = new StringWriter();
-            TransformerFactory tf = TransformerFactory.newInstance();
-            Transformer transformer = tf.newTransformer();
-            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-
-            transformer.transform(new DOMSource(doc), new StreamResult(sw));
-            return sw.toString();
-        } catch (Exception ex) {
-            throw new RuntimeException("Error converting to String", ex);
-        }
+        return options;
     }
 
 }
--- src/main/tests/source/ApacheXmlTest.java
+++ src/main/tests/source/ApacheXmlTest.java
@@ -1,12 +1,15 @@
 import org.junit.Test;
 import com.kalashnikov.monitoring.config.apache.xml.Configurator;
 
-public class ApacheTest {
+import java.util.Map;
+
+public class ApacheXmlTest {
 
     @Test
     public void performTest() {
         Configurator configurator = new Configurator();
-        System.out.println(configurator.xmlSimpleReader());
+        Map map = configurator.xmlConfiguratorReader();
+        System.out.println(map.entrySet());
     }
 
 }
