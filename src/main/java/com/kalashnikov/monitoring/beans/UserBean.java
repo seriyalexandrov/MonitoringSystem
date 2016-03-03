ae38d3f6847faaeeb1a428f8b5dea05c74c16db8
yazh2009 <yazh2009@rambler.ru>
2016-03-04 02:01:43 +0300

    Entities and bean
    Added entities and bean, edited persistence.xml

---
+++ src/main/java/com/kalashnikov/monitoring/beans/UserBean.java
@@ -0,0 +1,37 @@
+package com.kalashnikov.monitoring.beans;
+
+import com.kalashnikov.monitoring.entities.UsersEntity;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Stateless
+public class UserBean {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public UsersEntity saveNewUser(UsersEntity user){
+        return entityManager.merge(user);
+    }
+
+    public void updateUser(UsersEntity user){
+        saveNewUser(user);
+    }
+
+    public UsersEntity getUser(int userId){
+        return entityManager.find(UsersEntity.class, userId);
+    }
+
+    public void deleteUser(int userId){
+        entityManager.remove(getUser(userId));
+    }
+
+    public List<UsersEntity> getAllUsers(){
+        TypedQuery<UsersEntity> namedQuery = entityManager.createNamedQuery("getAllUsers", UsersEntity.class);
+        return namedQuery.getResultList();
+    }
+
+}
---
+++ src/main/java/com/kalashnikov/monitoring/entities/UsersEntity.java
@@ -0,0 +1,67 @@
+package com.kalashnikov.monitoring.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS", schema = "PUBLIC", catalog = "GLASSFISHDB")
+@NamedQueries({
+        @NamedQuery(name = "getAllUsers", query = "SELECT u FROM UsersEntity u"),
+        @NamedQuery(name = "getUser", query = "SELECT u FROM UsersEntity u WHERE u.userName='stapko'")
+})
+public class UsersEntity {
+    private int userId;
+    private String userName;
+    private String password;
+
+    @Id
+    @Column(name = "USER_ID")
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "USER_NAME")
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Basic
+    @Column(name = "PASSWORD")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsersEntity that = (UsersEntity) o;
+
+        if (userId != that.userId) return false;
+        if (userName != null ? !userName.equals(that.userName) : that.userName != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId;
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+}
