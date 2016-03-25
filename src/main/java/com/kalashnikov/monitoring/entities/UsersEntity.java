14649c99ff5131d94aab5a08f37268da3d7a0a81
yazh2009 <yazh2009@rambler.ru>
2016-03-26 00:10:35 +0300

    Work with jdbcRealm
    Returned SettingsEntity, edited web.xml, created Glassfish descriptor

--- src/main/java/com/kalashnikov/monitoring/entities/UsersEntity.java
+++ src/main/java/com/kalashnikov/monitoring/entities/UsersEntity.java
@@ -2,47 +2,67 @@ package com.kalashnikov.monitoring.entities;
 
 import javax.persistence.*;
 
-
 @Entity
 @Table(name = "USERS", schema = "PUBLIC", catalog = "GLASSFISHDB")
-@NamedQuery(name = "UsersEntity.getAll", query = "Select u from UsersEntity u")
+@NamedQueries({
+        @NamedQuery(name = "getAllUsers", query = "SELECT u FROM UsersEntity u"),
+        @NamedQuery(name = "getUser", query = "SELECT u FROM UsersEntity u WHERE u.userName='stapko'")
+})
 public class UsersEntity {
-    public UsersEntity() {
-
-    }
+    private int userId;
+    private String userName;
+    private String password;
+    private Integer groupId;
+    private String groupName;
 
     @Id
-    @Column(name = "USERID")
-    private int userId;
-
+    @Column(name = "USER_ID")
     public int getUserId() {
         return userId;
     }
 
-    public void setUserId(int userid) {
-        this.userId = userid;
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 
-    @Column(name = "USERNAME")
-    private String userName;
-
+    @Basic
+    @Column(name = "USER_NAME")
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String username) {
-        this.userName = username;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
+    @Basic
     @Column(name = "PASSWORD")
-    private String password;
-
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Basic
+    @Column(name = "GROUP_ID")
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    @Basic
+    @Column(name = "GROUP_NAME")
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
     }
 
     @Override
@@ -55,6 +75,8 @@ public class UsersEntity {
         if (userId != that.userId) return false;
         if (userName != null ? !userName.equals(that.userName) : that.userName != null) return false;
         if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (groupId != null ? !groupId.equals(that.groupId) : that.groupId != null) return false;
+        if (groupName != null ? !groupName.equals(that.groupName) : that.groupName != null) return false;
 
         return true;
     }
@@ -64,6 +86,8 @@ public class UsersEntity {
         int result = userId;
         result = 31 * result + (userName != null ? userName.hashCode() : 0);
         result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (groupId != null ? groupId.hashCode() : 0);
+        result = 31 * result + (groupName != null ? groupName.hashCode() : 0);
         return result;
     }
 }
